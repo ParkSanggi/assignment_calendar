6f02736c239e90e5ec23334e9e5e263d6724ac4e
ParkSanggi <roqkfwkehlwk@naver.com>
2019-10-02 23:18:03 +0900

    add url schedule_ data

--- schedule/urls.py
+++ schedule/urls.py
@@ -4,7 +4,8 @@ from .views import *
 app_name = 'schedule'
 
 urlpatterns = [
-    path('monthly/', MakeMonthly.as_view(),),
+    path('monthly/', MakeMonthly.as_view()),
     path('monthly/<int:year>/<int:month>/<int:day>/', MakeMonthly.as_view(), name='monthly'),
+    path('schedule_data/', ScheduleListView.as_view(), name='schedule_data'),
 
 ]
