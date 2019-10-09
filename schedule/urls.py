f6fa9d558325b98443606c87ec1138faf2486494
ParkSanggi <roqkfwkehlwk@naver.com>
2019-10-09 17:36:10 +0900

    add daily path

--- schedule/urls.py
+++ schedule/urls.py
@@ -6,6 +6,7 @@ app_name = 'schedule'
 urlpatterns = [
     path('', MakeMonthly.as_view(), name='monthly_basic'),
     path('<int:year>/<int:month>/<int:day>/', MakeMonthly.as_view(), name='monthly'),
-    path('daily/', MakeDailyListView.as_view(), name='daily'),
+    path('daily/', MakeDailyListView.as_view(), name='daily_basic'),
+    path('daily/<int:month>/<int:day>/<int:year>/', MakeDailyListView.as_view(), name='daily'),
     path('create_schedule/', CreateScheduleView.as_view(), name='create_schedule'),
 ]
